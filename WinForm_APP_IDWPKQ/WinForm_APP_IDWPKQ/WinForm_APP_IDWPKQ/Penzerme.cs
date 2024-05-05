158ec685effd6a909068d21c41d26ea1f04419d1
Bárdos Botond <bardosb02@gmail.com>
2024-05-05 12:10:00 +0200

    Projekt alapjai

---
+++ WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/Penzerme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_APP_IDWPKQ
+{
+    public partial class Penzerme : Form
+    {
+        public Penzerme()
+        {
+            InitializeComponent();
+        }
+    }
+}
