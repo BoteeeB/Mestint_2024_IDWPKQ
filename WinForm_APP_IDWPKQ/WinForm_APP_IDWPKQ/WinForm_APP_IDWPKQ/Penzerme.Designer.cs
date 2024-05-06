13f3881183a106d59c6347502fec789f9583661b
Bárdos Botond <bardosb02@gmail.com>
2024-05-06 16:31:13 +0200

    Játék alap megjelenítése, próbálgatás

--- WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/Penzerme.Designer.cs
+++ WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/Penzerme.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.JatekPanel = new System.Windows.Forms.Panel();
+            this.DefaultSolve = new System.Windows.Forms.Button();
+            this.DepthFirstSolve = new System.Windows.Forms.Button();
+            this.BreadthFirstSolve = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // JatekPanel
+            // 
+            this.JatekPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.JatekPanel.Location = new System.Drawing.Point(51, 64);
+            this.JatekPanel.Name = "JatekPanel";
+            this.JatekPanel.Size = new System.Drawing.Size(400, 400);
+            this.JatekPanel.TabIndex = 0;
+            // 
+            // DefaultSolve
+            // 
+            this.DefaultSolve.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DefaultSolve.Location = new System.Drawing.Point(508, 64);
+            this.DefaultSolve.Name = "DefaultSolve";
+            this.DefaultSolve.Size = new System.Drawing.Size(184, 94);
+            this.DefaultSolve.TabIndex = 1;
+            this.DefaultSolve.Text = "BACKTRACK";
+            this.DefaultSolve.UseVisualStyleBackColor = true;
+            this.DefaultSolve.Click += new System.EventHandler(this.DefaultSolve_Click);
+            // 
+            // DepthFirstSolve
+            // 
+            this.DepthFirstSolve.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DepthFirstSolve.Location = new System.Drawing.Point(508, 217);
+            this.DepthFirstSolve.Name = "DepthFirstSolve";
+            this.DepthFirstSolve.Size = new System.Drawing.Size(184, 93);
+            this.DepthFirstSolve.TabIndex = 2;
+            this.DepthFirstSolve.Text = "DEPTH FIRST";
+            this.DepthFirstSolve.UseVisualStyleBackColor = true;
+            this.DepthFirstSolve.Click += new System.EventHandler(this.DepthFirstSolve_Click);
+            // 
+            // BreadthFirstSolve
+            // 
+            this.BreadthFirstSolve.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BreadthFirstSolve.Location = new System.Drawing.Point(508, 370);
+            this.BreadthFirstSolve.Name = "BreadthFirstSolve";
+            this.BreadthFirstSolve.Size = new System.Drawing.Size(184, 94);
+            this.BreadthFirstSolve.TabIndex = 3;
+            this.BreadthFirstSolve.Text = "BREADTH FIRST";
+            this.BreadthFirstSolve.UseVisualStyleBackColor = true;
+            this.BreadthFirstSolve.Click += new System.EventHandler(this.BreadthFirstSolve_Click);
             // 
             // Penzerme
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(881, 532);
+            this.Controls.Add(this.BreadthFirstSolve);
+            this.Controls.Add(this.DepthFirstSolve);
+            this.Controls.Add(this.DefaultSolve);
+            this.Controls.Add(this.JatekPanel);
             this.Name = "Penzerme";
             this.Text = "Pénzérmés játék";
             this.ResumeLayout(false);
@@ -42,6 +91,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel JatekPanel;
+        private System.Windows.Forms.Button DefaultSolve;
+        private System.Windows.Forms.Button DepthFirstSolve;
+        private System.Windows.Forms.Button BreadthFirstSolve;
     }
 }
 
--- WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/Penzerme.cs
+++ WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/WinForm_APP_IDWPKQ/Penzerme.cs
@@ -12,9 +12,58 @@ namespace WinForm_APP_IDWPKQ
 {
     public partial class Penzerme : Form
     {
+        private Penzerme_State game;
+
         public Penzerme()
         {
             InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            game = new Penzerme_State();
+            UpdatePanel(JatekPanel);
+        }
+
+        public void UpdatePanel(Panel panel)
+        {
+            panel.Controls.Clear();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Button button = new Button();
+                    button.Width = button.Height = 100;
+                    button.Top = i * 100;
+                    button.Left = j * 100;
+                    button.Enabled = false;
+                    if (i == 1 && j == 1 || i == 1 && j == 2 || i == 2 && j == 1 || i == 2 && j == 2)
+                    {
+                        button.Font = new Font(button.Font.FontFamily, 37, FontStyle.Bold); // Szöveg formázása
+                        button.Text = "O";
+                    }
+                    panel.Controls.Add(button);
+                }
+            }
+        }
+
+
+        private void DefaultSolve_Click(object sender, EventArgs e)
+        {
+            // ez a backtrack
+        }
+
+        private void DepthFirstSolve_Click(object sender, EventArgs e)
+        {
+            // ez a depthfirst
+        }
+
+        private void BreadthFirstSolve_Click(object sender, EventArgs e)
+        {
+            // ez a breadthfirst
         }
     }
+
 }
